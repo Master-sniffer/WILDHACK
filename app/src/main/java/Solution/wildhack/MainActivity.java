934540c49b43ba79ec99752e19ad2ef02b064cdf
Master_Sniffer <dsolodennikov@gmail.com>
2021-12-04 01:46:59 +0300

    Deleted not working shit from the repo

--- app/src/main/java/Solution/wildhack/MainActivity.java
+++ app/src/main/java/Solution/wildhack/MainActivity.java
@@ -4,20 +4,27 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.io.FileNotFoundException;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String files = "Потребуется стандартное походное снаряжение, за исключением палатки и посуды для приготовления еды на костре\n" +
+                "высокие резиновые сапоги («болотники»), спальник, коврик, налобный фонарик, термос, личная посуда\n" +
+                "(на некоторых кордонах вся посуда есть),\n" +
+                "биоразлагаемые средства гигиены, репелленты.\n" +
+                "Погода на Камчатке быстро и часто меняется – не забудьте про теплую и водоотталкивающую одежду и обувь.\n" +
+                "В теплые дни из соображений этики нельзя ходить по кордону в купальнике и коротких шортах,\n" +
+                "поэтому комплект легкой одежды тоже возьмите с собой. Помните, вы – лицо заповедника, наравне с сотрудниками!";
+        String flows = "Привет\n" +
+                "Пока\n" +
+                "Ага\n" +
+                "Вот\n" +
+                "да\n" +
+                "пока\n";
 
-        try {
-            Tests.tests();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-
+        Tests.tests(files);
+        Tests.tests(flows);
     }
 }
