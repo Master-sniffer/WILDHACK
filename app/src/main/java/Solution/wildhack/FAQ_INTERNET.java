23edb270d0bff292f4409fc0056af57c968284e3
Master_Sniffer <dsolodennikov@gmail.com>
2021-12-05 13:22:06 +0300

    Last Update

--- app/src/main/java/Solution/wildhack/FAQ_INTERNET.java
+++ app/src/main/java/Solution/wildhack/FAQ_INTERNET.java
@@ -1,6 +1,9 @@
 package Solution.wildhack;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,8 +16,10 @@ public class FAQ_INTERNET extends AppCompatActivity {
 
     }
 
+    public void Go_To_Youtube (View view){
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=Zv4hIaVvFVI&ab_channel=вДудь"));
+        startActivity(browserIntent);
+    }
 
 
-    //Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=Zv4hIaVvFVI&ab_channel=вДудь"));
-    //startActivity(browserIntent);
 }
